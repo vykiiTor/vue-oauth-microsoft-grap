e0305af42ab16c3e324394a2205d6b12eb150bbd
victor.tran <victor.tran@universite-paris-saclay.fr>
2022-09-26 16:07:40 +0200

    Ex 4: create HomePage component

---
+++ src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+        <v-btn flat>Welcome To my basic HomePage</v-btn>
+</template>
+
+<script>
+export default {
+name: 'HomePage'
+};
+</script>
+
+<style scoped>
+</style>
